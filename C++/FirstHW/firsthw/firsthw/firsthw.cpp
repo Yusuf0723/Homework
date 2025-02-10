ff009aa37896ffe2a962a808f9e36464e07f02ac
USER <aliyevyusuf54@gmail>
2025-02-10 18:08:48 +0400

    First hw

---
+++ C++/FirstHW/firsthw/firsthw/firsthw.cpp
@@ -0,0 +1,12 @@
+﻿
+#include <iostream>
+using namespace std;
+int main() {
+	int y, x;
+	cout << "Enter y:" << endl;
+	cin >> y;
+	cout << "Enter x:" << endl;
+	cin >> x;
+	cout << pow(x, y) << endl;
+}
+
