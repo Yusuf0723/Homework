3c9f2b59c54202c29a9c96b7f3b482aecbd2ed25
USER <aliyevyusuf54@gmail>
2025-03-16 10:09:57 +0400

    Checking

---
+++ hw3/hw3/hw3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+void forth(int num[], int x) {
+        int sum = 0;
+        int i = 0;
+        while (i < x) {
+            sum += num[i];
+            i++;
+        }
+        cout << "Sum: " << sum << endl;
+    }
+    
+void third(int nums[], int x) {
+    int a = 0;
+    while (a < x) {
+        if (nums[a] % 2 == 0) {
+            cout << nums[a] << endl;
+        }
+        a++;
+    }
+}
+int main() {
+    int arr[] = { 12, 143, 5, 0, 23, 45, 233, 10, 7 };
+    third(arr, x);
+}
